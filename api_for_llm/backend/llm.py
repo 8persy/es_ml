4cb8f63cf5f64f242abf66a4820a7d44d551abd9
udazzZZ <pirogova.dv96@mail.ru>
2025-12-07 00:51:39 +0300

    feat: add langfuse

--- api_for_llm/backend/llm.py
+++ api_for_llm/backend/llm.py
@@ -8,15 +8,26 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 import requests
 import os
 from dotenv import load_dotenv
+from langfuse import observe, Langfuse
+
+os.environ["TRANSFORMERS_NO_TF"] = "1"
+os.environ["DISABLE_TF_IMPORT"] = "1"
 
 MODEL_NAME = "Qwen/Qwen2-1.5B-Instruct"
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '../../.env'))
+
 NEWS_API_KEY = os.getenv("API_KEY")
-print(NEWS_API_KEY)
-NEWS_API_URL = "https://newsapi.org/v2/everything"
+LANGFUSE_SECRET_KEY = os.getenv("LANGFUSE_SECRET_KEY")
+LANGFUSE_PUBLIC_KEY = os.getenv("LANGFUSE_PUBLIC_KEY")
+LANGFUSE_BASE_URL = os.getenv("LANGFUSE_BASE_URL")
+
+langfuse = Langfuse(
+    public_key=LANGFUSE_PUBLIC_KEY,
+    secret_key=LANGFUSE_SECRET_KEY,
+    base_url=LANGFUSE_BASE_URL
+)
 
 app = FastAPI()
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -32,6 +43,7 @@ model = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
 model.to(device)
 model.eval()
 
+NEWS_API_URL = "https://newsapi.org/v2/everything"
 
 class InRequest(BaseModel):
     text: str
@@ -39,9 +51,10 @@ class InRequest(BaseModel):
     temperature: Optional[float] = 0.0
 
 
+@observe(name="summarize-to-keyword", as_type="generation")
 def summarize_to_keyword(user_input: str, max_new_tokens: int = 8, temperature: float = 0.0) -> str:
+    """Сокращает текст до одного ключевого слова"""
     prompt = f"Ты - умный ассистент. Сократи это предложение до одного ключевого слова: {user_input}\nКлючевое слово:"
-
     inputs = tokenizer(prompt, return_tensors="pt", truncation=True).to(device)
 
     with torch.no_grad():
@@ -50,8 +63,8 @@ def summarize_to_keyword(user_input: str, max_new_tokens: int = 8, temperature: 
             max_new_tokens=max_new_tokens,
             do_sample=False,
             temperature=temperature,
-            eos_token_id=tokenizer.eos_token_id if tokenizer.eos_token_id is not None else None,
-            pad_token_id=tokenizer.eos_token_id if tokenizer.eos_token_id is not None else tokenizer.pad_token_id
+            eos_token_id=tokenizer.eos_token_id,
+            pad_token_id=tokenizer.eos_token_id
         )
 
     gen_tokens = outputs[:, inputs["input_ids"].shape[1]:]
@@ -64,7 +77,9 @@ def summarize_to_keyword(user_input: str, max_new_tokens: int = 8, temperature: 
     return keyword
 
 
+@observe(name="fetch-news", as_type="generation")
 def fetch_news(keyword: str):
+    """Возвращает новости по ключевому слову"""
     params = {
         "q": keyword,
         "language": "ru",
@@ -87,13 +102,14 @@ def fetch_news(keyword: str):
         }
         for a in data["articles"]
     ]
-    return {"news": articles,"keyword": keyword}
+    return {"news": articles, "keyword": keyword}
 
 
 @app.post("/summarize")
 def api_summarize(req: InRequest):
-    kw = summarize_to_keyword(req.text, max_new_tokens=req.max_new_tokens, temperature=req.temperature)
-    news = fetch_news(kw)
+    """Эндпоинт: получение ключевого слова + новостей"""
+    keyword = summarize_to_keyword(req.text, max_new_tokens=req.max_new_tokens, temperature=req.temperature)
+    news = fetch_news(keyword)
     return news
 
 
